2db8588c39d42efb36eff3d48ee53c75b9388cc1
snowzhu <snowzhu@tencent.com>
2022-11-19 17:41:43 +0800

    feat:update

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -24,6 +24,15 @@ M.config = function()
 				},
 			},
 		},
+		inactive_sections = {
+			lualine_a = {
+				{ 'branch',
+					color = { bg = "#e39527", fg = "#000000", gui = 'bold' },
+					separator = { right = '' },
+					left_padding = 2,
+				},
+			},
+		},
 	}
 
 	--  lualine theme设置
