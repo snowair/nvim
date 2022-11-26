ab8ba736d4c7081f4f1af6e6acac362867aaa2fb
zhuyajie <930664@qq.com>
2022-11-26 23:35:14 +0800

    update

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -4,25 +4,30 @@ local Job = require 'plenary.job'
 local notify = require 'notify'
 local stateCache = {
 	color = "white",
-	state = '   ',
+	state = '  ',
+	staged = 0,
+	unstaged = 0,
+	unmerged = 0,
+	untracked = 0,
+	status = 0,
 }
 
-local function checkLocalCommit(info,cwd)
+local function checkLocalCommit(info, cwd)
 	Job:new({
 		command = 'git',
 		args = { 'rev-parse', '--short', 'HEAD' },
 		cwd = cwd,
-		env = {PATH=vim.env.PATH},
+		env = { PATH = vim.env.PATH },
 		on_exit = function(j, _)
 			if j.code == 0 then
 				if #j._stdout_results == 1 then
 					local current_commit = j._stdout_results[1]
 					local pos = string.find(info, current_commit)
 					if pos ~= nil and pos >= 1 then
-						stateCache.state = '  '
+						stateCache.state = ' '
 						stateCache.color = "red"
 					else
-						stateCache.state = "   "
+						stateCache.state = "  "
 						stateCache.color = "red"
 					end
 				end
@@ -36,7 +41,7 @@ local function getForwardNum(cwd)
 		command = 'git',
 		args = { 'rev-list', 'HEAD', '--not', '--remotes' },
 		cwd = cwd,
-		env = {PATH=vim.env.PATH},
+		env = { PATH = vim.env.PATH },
 		on_exit = function(j, _)
 			if j.code == 0 then
 				local forward = #j._stdout_results
@@ -44,13 +49,50 @@ local function getForwardNum(cwd)
 					stateCache.state = "  (" .. tonumber(forward) .. ")"
 					stateCache.color = "orange"
 				else
-					stateCache.state = "   "
+					stateCache.state = "  "
 					stateCache.color = "green"
 				end
 			end
 		end,
 	}):start()
 
+end
+
+local function setStatus(cwd)
+	Job:new({
+		command = 'git',
+		args = { 'status', '--short' },
+		cwd = cwd,
+		env = { PATH = vim.env.PATH },
+		on_exit = function(j, _)
+			if j.code == 0 then
+				stateCache.status = 1
+				local unmerged = 0
+				local unstaged = 0
+				local untracked = 0
+				local staged = 0
+				for _, line in ipairs(j._stdout_results) do
+					local a = string.sub(line, 1, 1)
+					local b = string.sub(line, 2, 2)
+					if a == "?" and b == "?" then
+						untracked = 1 + untracked
+					elseif a ~= " " and b ~= " " then
+						unmerged = 1 + unmerged
+					elseif a ~= " " and b == " " then
+						staged = 1 + staged
+					elseif a == " " and b ~= " " then
+						unstaged = 1 + unstaged
+					end
+				end
+				stateCache.unmerged  = unmerged
+				stateCache.staged    = staged
+				stateCache.unstaged  = unstaged
+				stateCache.untracked = untracked
+			else
+				stateCache.status = 0
+			end
+		end,
+	}):start()
 end
 
 local timer = vim.loop.new_timer()
@@ -60,12 +102,12 @@ M.gitStatusTaskFn = function()
 		return
 	end
 	runing = true
-	local cwd =vim.loop.cwd()
+	local cwd = vim.loop.cwd()
 	Job:new({
 		command = 'git',
 		args = { 'fetch', '--dry-run' },
 		cwd = cwd,
-		env = {PATH=vim.env.PATH},
+		env = { PATH = vim.env.PATH },
 		on_exit = function(j, _)
 			if j.code == 0 then
 				if #j._stderr_results == 0 then
@@ -73,12 +115,14 @@ M.gitStatusTaskFn = function()
 					local function tmp()
 						getForwardNum(cwd)
 					end
+
 					vim.schedule_wrap(tmp)()
 				else
 					-- 有远程更新
 					local function tmp()
-						checkLocalCommit(j._stderr_results[2],cwd)
+						checkLocalCommit(j._stderr_results[2], cwd)
 					end
+
 					vim.schedule_wrap(tmp)()
 				end
 			else
@@ -87,12 +131,42 @@ M.gitStatusTaskFn = function()
 			runing = false
 		end,
 	}):start()
+
+	setStatus(cwd)
 end
 
-timer:start(0, 3000, vim.schedule_wrap(M.gitStatusTaskFn))
+timer:start(0, 10000, vim.schedule_wrap(M.gitStatusTaskFn))
 
 local function update_branch_status()
 	return stateCache.state
+end
+
+local function update_index_status()
+	if stateCache.status == 0 then
+		return ""
+	end
+	local result = ""
+	if stateCache.unstaged > 0 then
+		result = string.format("us(%d)", stateCache.unstaged)
+	end
+
+	if stateCache.unmerged > 0 then
+		result = result .. string.format(" um(%d)", stateCache.unmerged)
+	end
+
+	if stateCache.untracked > 0 then
+		result = result .. string.format(" ut(%d)", stateCache.untracked)
+	end
+
+	result = vim.trim(result)
+
+	if stateCache.staged > 0 and result == "" then
+		return string.format("staged(%d)", stateCache.staged)
+	elseif stateCache.staged == 0 and result == "" then
+		return string.format("clean")
+	else
+		return result
+	end
 end
 
 M.config = function()
@@ -167,6 +241,7 @@ M.config = function()
 						return { fg = stateCache.color, gui = 'bold' }
 					end,
 				},
+				update_index_status,
 			},
 			lualine_x = { 'fileformat', },
 			lualine_y = { 'filetype', 'encoding', 'filesize', 'location' },
