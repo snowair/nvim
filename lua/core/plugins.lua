899a6b605677aa7eb1522a7da377d74411dfe6fe
zhuyajie <930664@qq.com>
2022-12-16 02:19:23 +0800

    update

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -942,6 +942,7 @@ return require('packer').startup({ function(use)
 				null_ls.builtins.code_actions.eslint,
 				null_ls.builtins.code_actions.gitrebase,
 				null_ls.builtins.code_actions.gitsigns,
+                null_ls.builtins.formatting.eslint,
 				--null_ls.builtins.completion.luasnip,
 				--null_ls.builtins.diagnostics.eslint,
 				--null_ls.builtins.diagnostics.jsonlint,
@@ -951,7 +952,6 @@ return require('packer').startup({ function(use)
 				--null_ls.builtins.formatting.tidy,
 			}
 			null_ls.setup({ sources = sources, debug = false })
-			null_ls.setup()
 			require("../configs/go_code_actions") -- 加载自定义的code_actions
 		end
 	}
