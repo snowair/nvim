15655b14e22784ad3bb4dd66cd77a1b7704f7da7
zhuyajie <930664@qq.com>
2023-04-18 23:32:11 +0800

    update:M  lua/configs/lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -45,7 +45,7 @@ function M.config()
           -- more settings: https://github.com/golang/tools/blob/master/gopls/doc/settings.md
           -- flags = {allow_incremental_sync = true, debounce_text_changes = 500},
           -- not supported
-          analyses = { unusedparams = true, unreachable = true },
+          analyses = { unusedparams = true, unreachable = true, unusedwrite=true,  },
           codelenses = {
             generate = true, -- show the `go generate` lens.
             gc_details = true, --  // Show a code lens toggling the display of gc's choices.
@@ -55,10 +55,10 @@ function M.config()
           usePlaceholders = true,
           completeUnimported = true,
           staticcheck = true,
-          matcher = 'fuzzy',
+          matcher = 'FastFuzzy',
           diagnosticsDelay = '500ms',
           experimentalWatchedFileDelay = '1000ms',
-          symbolMatcher = 'fuzzy',
+          symbolMatcher = 'FastFuzzy',
           gofumpt = false, -- true, -- turn on for new repos, gofmpt is good but also create code turmoils
           buildFlags = { '-tags', 'integration' },
         },
