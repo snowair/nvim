5a900579498e8fa3cf4ea9a71e6311b745aae7ad
snowair <15101230@qq.com>
2023-07-01 23:04:21 +0800

    lua/simple/keymaps.lua

--- lua/simple/keymaps.lua
+++ lua/simple/keymaps.lua
@@ -18,108 +18,24 @@ vim.keymap.set('t', '<f15>', '<c-\\><c-n>:FloatermToggle<return>')
 vim.keymap.set('t', '<f27>', '<c-\\><c-n>:FloatermNext<return>')
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
 
--- telescope
-local function get_dirs()
-  local ssdir = vim.env.SESSION_DIR
-  local dirlist = vim.g.dirChangeHistory
-  local cwd = vim.loop.cwd()
-  if dirlist ~= nil and #dirlist >= 1 then
-    -- cdlist有可能不含有当前cwd,所以需要加入
-    local cwd_exsist = false
-    for _, v in pairs(dirlist) do
-      if v == cwd then
-        cwd_exsist = true
-      end
-    end
-
-    if cwd_exsist == false then
-      table.insert(dirlist, cwd)
-    end
-
-    local dirlist_copy = {}
-    local inited = false
-    for _, v in pairs(dirlist) do
-      if inited == false then
-        table.insert(dirlist_copy, v)
-        inited = true
-      else
-        local breaked = false
-        for iCopy, copyVal in pairs(dirlist_copy) do
-          local start, _ = string.find(copyVal, v)
-          if start == 1 then -- v更短
-            table.insert(dirlist_copy, v)
-            table.remove(dirlist_copy, iCopy)
-            breaked = true
-            break
-          end
-
-          local start2, _ = string.find(v, copyVal)
-          if start2 == 1 then -- copyVal 更短
-            breaked = true
-            break
-          end
-        end
-
-        if breaked == false then
-          table.insert(dirlist_copy, v)
-        end
-      end
-    end
-    return dirlist_copy
-  else
-    if ssdir ~= nil then
-      return ssdir
-    else
-      return cwd
-    end
-  end
-end
-
--- 在cdlist中搜文件内容
+-- 搜文件内容
 local function live_grep_in_cdlist()
-  require 'telescope.builtin'.live_grep({ search_dirs = get_dirs() })
+  require 'telescope.builtin'.live_grep()
 end
 vim.keymap.set({ 'n', 'i' }, '<c-F9>', live_grep_in_cdlist)
 vim.keymap.set({ 'n', 'i' }, '<F33>', live_grep_in_cdlist)
 
--- 在cdlist中搜文件
-local function find_files_in_cdlist()
-  require 'telescope.builtin'.find_files({ search_dirs = get_dirs() })
-end
-vim.keymap.set({ 'n', 'i' }, '<c-F10>', find_files_in_cdlist)
-vim.keymap.set({ 'n', 'i' }, '<F34>', find_files_in_cdlist)
-
--- 在session dir中搜文件
+-- 搜文件
 local function find_files_in_session_dir()
-  local ssdir = vim.env.SESSION_DIR
-  if ssdir ~= nil then
-    require 'telescope.builtin'.find_files({ search_dirs = { ssdir } })
-  else
-    require 'telescope.builtin'.find_files()
-  end
+  require 'telescope.builtin'.find_files()
 end
 vim.keymap.set({ 'n', 'i' }, '<c-F11>', find_files_in_session_dir)
 vim.keymap.set({ 'n', 'i' }, '<F35>', find_files_in_session_dir)
 
--- 在session dir中搜文件内容
-local function live_grep_in_session_dir()
-  local ssdir = vim.env.SESSION_DIR
-  if ssdir ~= nil then
-    require 'telescope.builtin'.live_grep({ search_dirs = { ssdir } })
-  else
-    require 'telescope.builtin'.live_grep()
-  end
-end
-vim.keymap.set({ 'n', 'i' }, '<s-F11>', live_grep_in_session_dir)
-vim.keymap.set({ 'n', 'i' }, '<F23>', live_grep_in_session_dir)
-
-vim.keymap.set({ 'n', 'i' }, '<c-F12>', function() require 'telescope.builtin'.buffers({}) end) -- 搜buffer
-vim.keymap.set({ 'n', 'i' }, '<F36>', function() require 'telescope.builtin'.buffers({}) end) -- 搜buffer
-vim.keymap.set({ 'n' }, '<c-space>', function()
-  vim.cmd("silent! noa wall")
-  require 'configs.bufferlist'.run({ only_cwd = true })
-end)
-
+-- 搜buffer
+vim.keymap.set({ 'n', 'i' }, '<c-F12>', function() require 'telescope.builtin'.buffers({}) end)   -- 搜buffer
+vim.keymap.set({ 'n', 'i' }, '<F36>', function() require 'telescope.builtin'.buffers({}) end)     -- 搜buffer
+vim.keymap.set({ 'n', 'i' }, '<c-space>', function() require 'telescope.builtin'.buffers({}) end) -- 搜buffer
 
 -- 剪贴板操作
 vim.keymap.set('v', '<C-Insert>', '"+y')
