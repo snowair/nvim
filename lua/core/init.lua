1fbd570f7e748b6fc624010c8c86c4bb38d445f9
snowair <15101230@qq.com>
2023-09-17 00:36:56 +0800

    lua/core/init.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -49,7 +49,7 @@ local alpha = function()
   return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
 end
 -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-vim.g.neovide_transparency = 0.0
+vim.g.neovide_transparency = 0.9
 vim.g.transparency = 0.9
 vim.g.neovide_background_color = "#0f1117" .. alpha()
 
@@ -80,8 +80,8 @@ require("core.theme")
 -- vim-markdown配置
 vim.g.vim_markdown_toc_autofit = 1
 vim.g.vim_markdown_fenced_languages = {
-    'js=javascript', 'py=python', 'golang=go', 'c++=cpp', 'viml=vim', 'bash=sh', 'ini=dosini', 'ts=typescript',
-    'md=markdown', 'cs=csharp', 'php', 'rb=ruby'
+  'js=javascript', 'py=python', 'golang=go', 'c++=cpp', 'viml=vim', 'bash=sh', 'ini=dosini', 'ts=typescript',
+  'md=markdown', 'cs=csharp', 'php', 'rb=ruby'
 }
 vim.g.vim_markdown_folding_level = 3
 vim.g.vim_markdown_frontmatter = 1
