c7bc4d5a1e677074d5c1f67e466f4e32859bc775
snowair <15101230@qq.com>
2024-05-26 22:58:43 +0800

    lua/core/plugins.lua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1505,6 +1505,22 @@ return require('packer').startup({
     }
 
     use "Pocco81/HighStr.nvim"
+
+    use "tpope/vim-dadbod"
+    use "kristijanhusak/vim-dadbod-completion"
+    use "kristijanhusak/vim-dadbod-ui"
+
+    use {
+      'Exafunction/codeium.vim',
+      config = function()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set('i', '<a-j>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+        vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+        vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+        vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+      end
+    }
+
   end,
 
   config = { max_jobs = 5 }
