f128b625d4692cfc0d5e282c687750dcabfe4596
snowzhu <snowzhu@tencent.com>
2024-11-14 14:57:52 +0800

    --other=Modified: .gitignore lua/core/plugins.lua lua/core/theme.lua

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -51,7 +51,7 @@ require('nightfox').setup({
     -- Compiled file's destination location
     compile_path = vim.fn.stdpath("cache") .. "/nightfox",
     compile_file_suffix = "_compiled", -- Compiled file suffix
-    transparent = true,                -- Disable setting background
+    transparent = false,                -- Disable setting background
     terminal_colors = true,            -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
     dim_inactive = false,              -- Non focused panes set to alternative background
     styles = {                         -- Style to be applied to different syntax groups
@@ -86,7 +86,7 @@ require("catppuccin").setup({
     shade = "dark",
     percentage = 0.15,
   },
-  transparent_background = true,
+  transparent_background = false,
   term_colors = false,
   compile = {
     enabled = false,
